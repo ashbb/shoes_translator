042e108da298506859f320f50c29ea0d758f6c36
ashbb <ashbbb@gmail.com>
2009-03-02 00:35:31 +0900

    first release 0.0.5

---
+++ README.md
@@ -0,0 +1,28 @@
+Shoes Translator
+================
+
+This is a tiny Shoes app. It works as like as the Mouseover Dictionary.
+
+Usage:
+------
+	cd shoes_translator/lib
+	shoes shoes_translator.rb
+
+- select a language
+- click `paste` button, then will be pasted from the system clipboard
+- show the translation word as mouse hovering
+- if not showing anything, click the word
+
+Change log:
+-----------
+Mar 01st, 2009: First release 0.0.5
+
+To do list:
+-----------
+- replace `paste` button to another way
+- use the right font for each language
+
+The last:
+---------
+Have fun! <br>
+**ashbb**
---
+++ lib/shoes_translator.rb
@@ -0,0 +1,83 @@
+# shoes_translator.rb
+require 'open-uri'
+
+PATH = "http://ajax.googleapis.com/ajax/services/language/" \
+       "translate?v=1.0&langpair=en%7C"
+Q = "&q="
+
+AW, AH = 200, 30
+
+class Translator < Shoes
+  url '/', :index
+  url '/translate', :translate
+  
+  def index
+    background lightblue..limegreen, :angle => 90
+    para strong "Select a language (click)\n\n"
+    IO.readlines('../data/languages.txt').each do |line|
+      country, lang = line.split ','
+      b = nil
+      f = flow do
+        b = background(gold, :curve => 5).hide
+        para country
+      end
+      f.style :width => 95
+      f.hover{b.show}
+      f.leave{b.hide}
+      f.click{$lang = lang.strip; visit '/translate'}
+    end
+  end
+  
+  def translate
+    background khaki..lightsalmon
+    make_words_with_flow
+    make_dic_area
+  end
+  
+  def make_words_with_flow
+    button('paste'){visit '/translate'}
+    
+    text = clipboard
+    words = text.split(' ')
+    flows = []
+  
+    words.each do |w|
+      w += ' '
+      flows << flow{para code(w)}
+      flows[-1].style :width => w.length * 10
+    end
+  
+    flows.each_with_index do |f, n|
+      blk = proc do
+        #debug  "#{words[n]} : #{f.left},#{f.top}"
+        mess = words[n].downcase.delete 'a-z'
+        word = words[n].delete mess
+        @msg.text =  search word
+        w = @msg.text.length * 7
+        f.left + w < width ?  x = f.left : x = f.left - w
+        @area.move x, f.top + 30
+        @area.style :width => w
+        @area.show
+      end
+      f.hover &blk
+      f.click &blk
+      f.leave{@area.hide}
+    end
+  end
+  
+  def make_dic_area
+    @area = stack :width => AW, :height => AH do
+      background yellowgreen, :curve => 5
+      @msg = para '', :font => "MS UI Gothic", :size => "xx-small"
+    end.hide
+  end
+  
+  def search word
+    word = 'unknown' if word.empty?
+    result = open(PATH + $lang + Q + word).read.split('"')[5]
+    result =  search word[0...-1] if result.empty?
+    return word + ': ' + result
+  end
+end
+
+Shoes.app :title => 'Shoes Translator 0.0.5', :width => 400, :height => 400
